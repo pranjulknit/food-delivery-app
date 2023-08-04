44f7765973825e2e3d511bd11a8f38e237832947
Pranjul Chaurasiya <pranjul.19641@knit.ac.in>
2023-08-04 17:12:39 +0530

    routes has been done

--- src/components/Restaurants/Restaurants.js
+++ src/components/Restaurants/Restaurants.js
@@ -2,24 +2,56 @@ import React from "react";
 
 import "./Restaurant.css";
 
+import Text from "../../common/Text/Text";
+import {
+  restaurantData
+} from "./constant";
 import Restaurant from "./restaurant/Restaurant";
-import { restaurantData } from "./constant";
-function Restaurants(){
-    return(
-     <div className="rests-container">
-             <p className="rests-header"> Restaurants Near By You.</p>
-             <div className="rests-restaurant">
-              {
-              restaurantData.map((value)=>(
-                <Restaurant image={value.image} name={value.name} location={value.location} rating={value.rating} price={value.price} time={value.time} safety = {value.safety}/>
-              ))
-              }
-              </div>
-             
 
-     </div>
+function Restaurants({
+  title
+}) {
+  return ( <
+    div className = "rests-container" >
+    <
+    Text text = {
+      title
+    }
+    variant = "h1" / >
+    <
+    div className = "rests-restaurant" > {
+      restaurantData.map((value) => ( <
+        Restaurant image = {
+          value.image
+        }
+        name = {
+          value.name
+        }
+        location = {
+          value.location
+        }
+        rating = {
+          value.rating
+        }
+        price = {
+          value.price
+        }
+        time = {
+          value.time
+        }
+        safety = {
+          value.safety
+        }
+        />
+      ))
+    } <
+    /div>
 
-    );
+
+    <
+    /div>
+
+  );
 
 }
 
